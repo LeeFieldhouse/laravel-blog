635826247a085a3aa1dd5b3135ad46a56b72c7d7
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-23 14:18:18 +0100

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -56,9 +56,11 @@
                                     @endforeach
                                 </span>
                                 <span><i class="far fa-comments"></i> <a href="#comments">12 Comments</a></span>
-                                @if(Auth::user()->id == $post->user->id)
-                                    <span><i class="far fa-edit"></i><a href="{{route('posts.edit', $post->slug)}}">Edit</a></span>
-                                @endif
+                                @auth
+                                    @if(Auth::user()->id == $post->user->id)
+                                        <span><i class="far fa-edit"></i><a href="{{route('posts.edit', $post->slug)}}">Edit</a></span>
+                                    @endif
+                                @endauth
                             </div>
 
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lectus lacus, rutrum sit amet placerat et, bibendum nec mauris. Duis molestie, purus eget placerat viverra, nisi odio gravida sapien, congue tincidunt nisl ante nec tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris.</p>
